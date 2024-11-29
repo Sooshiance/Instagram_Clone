50224b5ade18ba6a44449731b784108bc4bc9e62
sooshiance <s.katafract@gmail.com>
2024-11-29 18:29:02 +0330

    style: user app style fixed

--- backend/user/views.py
+++ backend/user/views.py
@@ -19,18 +19,9 @@ class RegisterView(APIView):
     def post(self, request):
         serializer = RegisterSerializer(data=request.data)
         if serializer.is_valid():
-            user = UserService.register_user(serializer.validated_data['username'], serializer.validated_data['password'])
-            UserService.send_otp(user)
+            user:User = UserService.register_user(serializer.validated_data['username'], serializer.validated_data['password'])
             return Response({"message": "User registered successfully"}, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-class VerifyOTPView(APIView):
-    def post(self, request):
-        user = UserService.get_user_by_username(request.data['username'])
-        if user and UserService.verify_otp(user, request.data['otp']):
-            return Response({"message": "OTP verified successfully"}, status=status.HTTP_200_OK)
-        return Response({"message": "Invalid OTP"}, status=status.HTTP_400_BAD_REQUEST)
 
 
 class PasswordResetRequestView(APIView):
@@ -40,6 +31,14 @@ class PasswordResetRequestView(APIView):
             UserService.create_password_reset_otp(user)
             return Response({"message": "Password reset OTP sent"}, status=status.HTTP_200_OK)
         return Response({"message": "User not found"}, status=status.HTTP_404_NOT_FOUND)
+
+
+class VerifyOTPView(APIView):
+    def post(self, request):
+        user = UserService.get_user_by_username(request.data['username'])
+        if user and UserService.verify_otp(user, request.data['otp']):
+            return Response({"message": "OTP verified successfully"}, status=status.HTTP_200_OK)
+        return Response({"message": "Invalid OTP"}, status=status.HTTP_400_BAD_REQUEST)
 
 
 class PasswordResetView(APIView):
